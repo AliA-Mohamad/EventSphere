6e3168507deb2ac600d4db6bd832f6a5d3701cf7
AliA-Mohamad <ali_efyhvc@hotmail.com>
2024-01-08 17:52:58 -0300

    DbContext

---
+++ EventSphere/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EventSphere.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+    public DbSet<> Usuarios { get; set; }
+    public DbSet<> Eventos { get; set; }
+
+}
